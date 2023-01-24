170a70766458f85d68f6fdc5c646fa0434e0b4eb
r.bukin <r.bukin@gmail.com>
2023-01-24 20:05:55 +0300

    back: auth/login

---
+++ back/src/app/api/security.py
@@ -0,0 +1,40 @@
+import os
+from fastapi_login import LoginManager
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login.exceptions import InvalidCredentialsException
+from app.schemas.users import UserLogin
+from datetime import timedelta
+from app.db.repository.users import get_by_username
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+manager = LoginManager(
+    SECRET_KEY, 
+    '/login',
+    default_expiry=timedelta(minutes=15),
+    use_cookie=True,
+    use_header=False
+)
+
+@manager.user_loader()
+async def get_user_by_name(username: str) -> UserLogin:
+    user = await get_by_username(username)
+    return user
+
+
+def hash_password(plaintext: str):
+    return manager.pwd_context.hash(plaintext)
+
+
+def verify_password(plaintext: str, hashed: str):
+    return manager.pwd_context.verify(plaintext, hashed)
+
+
+async def authenticate_user(username: str, password: str):
+    user = await get_by_username(username)
+    if not user:
+        raise InvalidCredentialsException
+    if not user.is_active:
+        raise InvalidCredentialsException
+    if not verify_password(password, user.password):
+        raise InvalidCredentialsException
+    return user
--- back/src/app/api/v1/routes/auth.py
+++ back/src/app/api/v1/routes/auth.py
@@ -1,47 +1,9 @@
-import os
 from fastapi import APIRouter, Depends, Response
-from fastapi_login import LoginManager
+from app.api.security import manager, authenticate_user
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi_login.exceptions import InvalidCredentialsException
 from app.schemas.users import Token, UserLogin
-from datetime import timedelta
-from app.db.repository.users import get_by_username
 
-SECRET_KEY = os.environ["SECRET_KEY"]
 router = APIRouter()
-
-manager = LoginManager(
-    SECRET_KEY, 
-    '/login',
-    default_expiry=timedelta(minutes=15),
-    use_cookie=True,
-    use_header=False
-)
-
-@manager.user_loader()
-async def get_user_by_name(username: str) -> UserLogin:
-    user = await get_by_username(username)
-    return user
-
-
-def hash_password(plaintext: str):
-    return manager.pwd_context.hash(plaintext)
-
-
-def verify_password(plaintext: str, hashed: str):
-    return manager.pwd_context.verify(plaintext, hashed)
-
-
-async def authenticate_user(username: str, password: str):
-    user = await get_by_username(username)
-    if not user:
-        raise InvalidCredentialsException
-    if not user.is_active:
-        raise InvalidCredentialsException
-    if not verify_password(password, user.password):
-        raise InvalidCredentialsException
-    return user
-
 
 @router.post('/login', status_code=201)
 async def login(response: Response, form_data: OAuth2PasswordRequestForm = Depends())-> map:
