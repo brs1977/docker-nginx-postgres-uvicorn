ee71000a5bde57d3c632746385b07da159aac8ba
r.bukin <r.bukin@gmail.com>
2023-01-25 09:30:47 +0300

    back: auth/login

--- back/src/tests/test_login.py
+++ back/src/tests/test_login.py
@@ -3,26 +3,6 @@ from app.schemas.users import UserSchema
 from app.api import security 
 
 from app.db.repository import users
-# import sys
-# import json
-# import asyncio
-# from app.db.repository import users
-# from app.server import app
-# from app.config import settings
-# from httpx import AsyncClient
-# from typing import Dict, Generator
-
-# accept: application/json
-# Accept-Encoding: gzip, deflate
-# Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7
-# Connection: keep-alive
-# Content-Length: 70
-# Content-Type: application/x-www-form-urlencoded
-# Cookie: access-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG0iLCJleHAiOjE2NzQ1OTYzNTR9.NdoRGBsrSmcQaEbapcq8LeDO_eTC1IQL0A3IkLzbpkI
-# Host: 129.200.0.116:8020
-# Origin: http://129.200.0.116:8020
-# Referer: http://129.200.0.116:8020/docs
-# User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36
 
 user_data = UserSchema(
     role_id=4,
@@ -88,42 +68,3 @@ def test_user_detail_forbidden_with_expired_token(test_app, api_url, freezer, mo
         # headers = {"Cookie": f"access-token={token}"}
     )
     assert response.status_code == 401
-
-
-# from loguru import logger
-# from app.utils.users import create_user, create_user_token
-# logger.add(sys.stdout)
-
-# @pytest.fixture
-# @pytest.mark.anyio
-# async def client() -> Generator:
-#     async with AsyncClient(app=app, base_url=settings.PROJECT_API_VERSION) as ac:
-#         yield ac
-
-
-# @pytest.mark.anyio
-# async def test_get_tokens(client: AsyncClient, api_url, monkeypatch) -> None:
-#     user_data = UserSchema(
-#         role_id=4,
-#         email="user@example.com",
-#         username="adm",
-#         password=security.hash_password("adm"),
-#         fio="string",
-#         is_active=True
-#     )
-
-#     async def mock_authenticate_user(username, password):
-#         return user_data
-#     monkeypatch.setattr(security, 'authenticate_user', mock_authenticate_user)
-
-#     request_data = {"username": "adm", "password": "adm"}
-#     request = await client.post("auth/login", data=request_data)
-#     assert request.status_code == 201
-
-# @pytest.fixture()
-# def mock_authenticate_user(monkeypatch):
-#     future = asyncio.Future()
-#     monkeypatch.patch('security.authenticate_user', return_value=future)
-#     return future
-
-
